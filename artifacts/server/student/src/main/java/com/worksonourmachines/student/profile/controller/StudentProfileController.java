95b9bfa441df7051f34a5d3d8609ae98f6979ed3
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2026-07-12 11:11:10 +0200

    add implementation placeholders in student microservice

--- artifacts/server/student/src/main/java/com/worksonourmachines/student/profile/controller/StudentProfileController.java
+++ artifacts/server/student/src/main/java/com/worksonourmachines/student/profile/controller/StudentProfileController.java
@@ -1,12 +1,16 @@
 package com.worksonourmachines.student.profile.controller;
 
 import org.openapitools.api.StudentApiV1;
+import org.openapitools.model.SharedStudentLearningGoal;
+import org.openapitools.model.SharedStudentLearningGoalInput;
 import org.openapitools.model.SharedStudentStudentProfile;
 import org.openapitools.model.SharedStudentStudentProfileInput;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.worksonourmachines.student.profile.service.StudentProfileService;
+
+import java.util.List;
 
 @RestController
 public class StudentProfileController implements StudentApiV1 {
@@ -18,8 +22,33 @@ public class StudentProfileController implements StudentApiV1 {
     }
 
     @Override
+    public ResponseEntity<SharedStudentLearningGoal> createGoal(SharedStudentLearningGoalInput sharedStudentLearningGoalInput) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteGoal(String id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SharedStudentLearningGoal> getGoal(String id) {
+        return null;
+    }
+
+    @Override
     public ResponseEntity<SharedStudentStudentProfile> getMyProfile() {
         return ResponseEntity.ok(studentProfileService.getCurrentStudentProfile());
+    }
+
+    @Override
+    public ResponseEntity<List<SharedStudentLearningGoal>> listMyGoals() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SharedStudentLearningGoal> updateGoal(String id, SharedStudentLearningGoalInput sharedStudentLearningGoalInput) {
+        return null;
     }
 
     @Override
